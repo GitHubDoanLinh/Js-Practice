be382262b8acf4417eda4bdb70b5e9da0a999c3d
Doan Huu Linh <doanlinh1510@gmail.com>
2024-05-10 16:42:33 +0700

    Additional code for primitives vs objects

--- 04-HowJsWork/PrimitivesvsObjects.js
+++ 04-HowJsWork/PrimitivesvsObjects.js
@@ -13,3 +13,28 @@ const friend = me;
 friend.age = 26;
 console.log('me: ', me);
 console.log('friend: ', friend);
+console.log("-------------------------------------")
+const jessica = {
+  firstName: "Jessica",
+  lastName: "Williams",
+  age:28
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'David';
+console.log('Before married:', jessica);
+console.log('After married:' ,marriedJessica);
+
+//Copying objects
+const linda = {
+  firstName: 'Linda',
+  lastName: 'Natalya',
+  age: 27,
+  family: ['Alice', 'Thane']
+};
+const lindaCopy = Object.assign({},linda);//only for Global scope
+lindaCopy.lastName = 'Dimitri';
+
+lindaCopy.family.push('Peter');
+lindaCopy.family.push('John');
+console.log('before: ', linda);
+console.log('after', lindaCopy);
