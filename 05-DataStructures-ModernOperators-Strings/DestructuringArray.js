289c9c692980aad9119c77f4522582d9530b1ae6
Doan Huu Linh <doanlinh1510@gmail.com>
2024-05-10 20:59:45 +0700

    .

--- 05-DataStructures-ModernOperators-Strings/DestructuringArray.js
+++ 05-DataStructures-ModernOperators-Strings/DestructuringArray.js
@@ -1,9 +1,4 @@
 'use strict';
-
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -28,5 +23,5 @@ const nested = [2,4,[5,6]];
 const [i, ,[j,k]] = nested;
 console.log(i,j,k);
 
-const [p=1, q=1, r=1] = [8,9];//Default values 
+const [p=1, q=1, r=1] = [8,9];//Default values
 console.log(p,q,r);
