5358ac954d0740282326cb36c63a05380f11c0e0
Doan Huu Linh <doanlinh1510@gmail.com>
2024-05-14 04:39:54 +0700

    .

--- 05-DataStructures-ModernOperators-Strings/FootballBet.js
+++ 05-DataStructures-ModernOperators-Strings/FootballBet.js
@@ -9,4 +9,25 @@ const game = {
   score: '3:0',
   scored: ['Muller', 'Gnabry', 'Lewandowski'],
   date: 'Dec 9th - 2023',
-}
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5
+  }
+};
+const [player1, player2] = game.players;
+console.log(player1, player2);
+const [gk, ...fieldPlayers] = player1;
+const allPlayers = [...player1, ...player2];
+const players1Final = [...player1, 'Thiago', 'Coutinho', 'Perisic'];
+const {
+  odds: { team1, x: draw, team2 }
+} = game;
+const printGoals = function(...players) {
+  console.log(`${players.length} goals were scored!`);
+  players.forEach(player => console.log(player));
+};
+printGoals('Davies', 'Kimmich', 'Goretzka');
+printGoals(...game.scored);
+team1 < team2 && console.log('Team1 will win 🏆!');
+team1 > team2 && console.log('Team2 will win 🏆!');
