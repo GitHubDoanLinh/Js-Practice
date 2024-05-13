c59b076dae176a366633ef78bb0305c5b9740ab0
Doan Huu Linh <doanlinh1510@gmail.com>
2024-05-13 23:48:37 +0700

    .

--- 05-DataStructures-ModernOperators-Strings/FootballBet.js
+++ 05-DataStructures-ModernOperators-Strings/FootballBet.js
@@ -1,6 +1,12 @@
 'use strict';
-const players1 = ['Neuer', 'Pavard', 'Sule', 'Hernandez', 'Davies', 'Kimmich', 'Goretzka', 'Muller', 'Gnabry', 'Lewandowski', 'Coman'];
-const players2 = ['Oblak', 'Trippier', 'Gimenez', 'Savic', 'Lodi', 'Koke', 'Llorente', 'Herrera', 'Carrasco', 'Felix', 'Suarez'];
-const [gk, ...fieldPlayers] = players1;
-const allPlayers = [...players1, ...players2];
-const players1Final = ['Thiago', 'Coutinho', 'Perisic',...players1];
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    ['Neuer', 'Pavard', 'Sule', 'Hernandez', 'Davies', 'Kimmich', 'Goretzka', 'Muller', 'Gnabry', 'Lewandowski', 'Coman'],
+    ['Oblak', 'Trippier', 'Gimenez', 'Savic', 'Lodi', 'Koke', 'Llorente', 'Herrera', 'Carrasco', 'Felix', 'Suarez']
+  ],
+  score: '4:0',
+  scored: ['Muller', 'Gnabry', 'Lewandowski', 'Coman'],
+  date: 'Dec 9th - 2023',
+}
