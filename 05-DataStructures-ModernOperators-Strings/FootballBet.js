fc533fbe967193ed484caac7e4137048e5212ef4
Doan Huu Linh <doanlinh1510@gmail.com>
2024-05-13 22:33:33 +0700

    .

---
+++ 05-DataStructures-ModernOperators-Strings/FootballBet.js
@@ -0,0 +1,6 @@
+'use strict';
+const players1 = ['Neuer', 'Pavard', 'Sule', 'Hernandez', 'Davies', 'Kimmich', 'Goretzka', 'Muller', 'Gnabry', 'Lewandowski', 'Coman'];
+const players2 = ['Oblak', 'Trippier', 'Gimenez', 'Savic', 'Lodi', 'Koke', 'Llorente', 'Herrera', 'Carrasco', 'Felix', 'Suarez'];
+const [gk, ...fieldPlayers] = players1;
+const allPlayers = [...players1, ...players2];
+const players1Final = ['Thiago', 'Coutinho', 'Perisic',...players1];
