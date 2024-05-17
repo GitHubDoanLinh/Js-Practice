ee9de418a46bb7483dab92f992a4eb088ee187a0
Doan Huu Linh <doanlinh1510@gmail.com>
2024-05-18 04:37:19 +0700

    array trong heap

---
+++ 02-Fundamentals-Part-2/starter/CompareArr.js
@@ -0,0 +1,7 @@
+'use strict';
+const a = [1, 2];
+const b = [1, 2];
+console.log(a === b); // false, vì a và b là hai đối tượng khác nhau trong bộ nhớ
+
+const c = a;
+console.log(a === c); // true, vì c và a tham chiếu đến cùng một đối tượng trong bộ nhớ
