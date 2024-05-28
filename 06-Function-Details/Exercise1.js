775ebf319d761d1ce11a0ba71c603438e10dba56
Doan Huu Linh <doanlinh1510@gmail.com>
2024-05-28 17:24:59 +0700

    illustrates how to use methods and events in JavaScript to create an interactive poll, register, and display the results of responses from users

--- 06-Function-Details/Exercise1.js
+++ 06-Function-Details/Exercise1.js
@@ -4,7 +4,7 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   answers: new Array(4).fill(0),
   registerNewAnswer() {
-    const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}(Write option number)`));
+    const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}(Write option number:)`));
     console.log(answer);
     //register answer
     typeof answer === 'number' && answer < this.options.length && this.answers[answer]++;
