60b78249613bb641ccc8373fc5b9b896414f0a32
Doan Huu Linh <doanlinh1510@gmail.com>
2024-05-28 17:12:04 +0700

    Exercise 1

---
+++ 06-Function-Details/Exercise1.js
@@ -0,0 +1,25 @@
+'use strict';
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}(Write option number)`));
+    console.log(answer);
+    //register answer
+    typeof answer === 'number' && answer < this.options.length && this.answers[answer]++;
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  }
+};
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// poll.displayResults.call()
