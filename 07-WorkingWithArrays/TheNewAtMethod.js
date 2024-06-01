acfd34008feda39c5d7f602a1cd062bbf9b55f8b
Doan Huu Linh <doanlinh1510@gmail.com>
2024-06-01 15:49:03 +0700

    lesson 2

---
+++ 07-WorkingWithArrays/TheNewAtMethod.js
@@ -0,0 +1,12 @@
+'use strict';
+const arr = [22, 23, 36];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+//Getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));//get 1 last element
+
+console.log('GitHub-DoanLinh'.at(0));
+console.log('GitHub-DoanLinh'.at(-1));
