15023640d56a56d1ca4797d1903a6eec5ae1c80a
Doan Huu Linh <doanlinh1510@gmail.com>
2024-06-05 16:31:32 +0700

    lesson 10 - The filter method

--- 07-WorkingWithArrays/ComputingUsernames.js
+++ 07-WorkingWithArrays/ComputingUsernames.js
@@ -36,13 +36,13 @@ console.log(username1);
 
 // refactoring
 const createUsernames = function(accs) {
-  accs.forEach(function(acc){
+  accs.forEach(function(acc) {
     acc.username = acc.owner
       .toLowerCase()
       .split(' ')
       .map(name => name[0])
       .join('');
-  })
-}
+  });
+};
 createUsernames(accounts);
 console.log(accounts);
