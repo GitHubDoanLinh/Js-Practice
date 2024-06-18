72a7536eda7b8624a749e63800587879412d1f36
Doan Huu Linh <doanlinh1510@gmail.com>
2024-06-18 22:02:18 +0700

    promise introduce

---
+++ AsynchronousInJS/Promise.js
@@ -0,0 +1,7 @@
+'use strict';
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v2/name/${country}`);
+// request.send();
+
+const request = fetch('https://restcountries.com/v2/name/vietnam');
+console.log(request);
