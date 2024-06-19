131c8d3931122951aaa1dfa299295e197e8fd1b8
Doan Huu Linh <doanlinh1510@gmail.com>
2024-06-19 22:55:38 +0700

    lesson 6 throw error

--- AsynchronousInJS/ThrowingError.js
+++ AsynchronousInJS/ThrowingError.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
         <div class='country__data'>
           <h3 class='country__name'>${data.name}</h3>
           <h4 class='country__region'>${data.region}</h4>
-          <p class='country__row'><span>👫</span>${(+data.population / 1000000).toFixed(1)} million people</p>
+          <p class='country__row'><span>👫</span>${(+data.population / 1000000).toFixed(1)} million peoples</p>
           <p class='country__row'><span>🗣️</span>${data.languages[0].name}</p>
           <p class='country__row'><span>💰</span>${data.currencies[0].name}</p>
         </div>
@@ -28,37 +28,41 @@ document.addEventListener('DOMContentLoaded', function() {
   const renderError = function(msg) {
     countriesContainer.insertAdjacentText('beforeend', msg);
     // countriesContainer.style.opacity = 1;
-  }
+  };
+  const getJSON = function(url, errorMsg = 'Something went wrong') {
+    return fetch(url).then(response => {
+      if (!response.ok)
+        throw new Error(`${errorMsg} ${response.status}`);
+
+      return response.json();
+    });
+  };
 
   const getCountryData = function(country) {
     // Country 1
-    fetch(`https://restcountries.com/v2/name/${country}`)
-      .then(
-        response => response.json())
+    getJSON(`https://restcountries.com/v2/name/${country}`, 'Country not found')
       .then(data => {
         renderCountry(data[0]);
         const neighbour = data[0].borders?.[0]; // Get data[0].borders[0];
+        // const neighbour = 'd.o.gnixson';
 
-        if (!neighbour) return;
+        if (!neighbour) throw new Error('No neighbour found!');
 
         // Country 2
-        return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
-      })
-      .then(response => {
-        if (!response) return;
-        return response.json();
+        return getJSON(`https://restcountries.com/v2/alpha/${neighbour}`, 'Country not found');
       })
       .then(data => renderCountry(data, 'neighbour'))
       .catch(err => {
         console.error(`${err.message}`);
-        renderError(`Something went wrong 💥💥💥 ${err.message}. Try again!`)
+        renderError(`Something went wrong 💥💥💥 ${err.message}. Try again!`);
       })
       .finally(() => {
         countriesContainer.style.opacity = 1;
-      })
+      });
   };
 
   btn.addEventListener('click', function() {
     getCountryData('vietnam');
   });
+  // getCountryData('kjbfpweibf')
 });
