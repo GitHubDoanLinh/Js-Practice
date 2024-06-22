f3b51d8cfec1356341e700c5fe20e1e95cbbb736
Doan Huu Linh <doanlinh1510@gmail.com>
2024-06-22 16:55:13 +0700

    exercise 2 (A series of images appear one after another)

--- AsynchronousInJS/exercise2.js
+++ AsynchronousInJS/exercise2.js
@@ -1,7 +1,67 @@
-'use strict';
-const createImage = function(imgPath) {
-  return new Promise(() => {
-    const getImg = document.createElement('img');
-    getImg.src
-  })
+const imgContainer = document.querySelector('.images');
+
+const wait = function(second) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, second * 1000);
+  });
 };
+const createImage = function(imgPath) {
+  return new Promise(function(resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function() {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function() {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+let currentImage;
+createImage('img/img-1.jpg')
+  .then(img => {
+    currentImage = img;
+    console.log('Image 1 loaded');
+    return wait(3);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    return createImage('img/img-2.jpg')
+  })
+  .then(img => {
+    currentImage = img;
+    console.log('Image 2 loaded');
+    return wait(3)
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    return createImage('img/img-3.jpg')
+  })
+  .then(img => {
+    currentImage = img;
+    console.log('Image 3 loaded');
+    return wait(3)
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    return createImage('img/myBabyNiece.jpg')
+  })
+  .then(img => {
+    currentImage = img;
+    console.log('image 4 loaded');
+    return wait(3);
+  })
+  .then(() => {
+    currentImage.style.display = 'none';
+    return createImage('img/myNiece.jpg')
+  })
+  .then(img => {
+    currentImage = img;
+    console.log('image 5 loaded');
+    return wait(3);
+  })
+  .catch(err => console.log(err));
